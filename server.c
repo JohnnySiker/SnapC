cc7c43a02380a4bf8f058b65285cdbfbe9048ee3
Jonathan Velazquez <jonhysiker@icloud.com>
2016-03-24 12:25:10 -0600

    Creando Archivos

---
+++ server.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+
+#define PORT 5012
+
+
+
+int main(int argc, char const *argv[]){
+	
+	return 0;
+}
